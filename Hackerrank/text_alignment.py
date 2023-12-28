c1d7632c44c389776db78db25aa96156488d873f
SidharthK-5 <sidharthsidzed55@gmail.com>
2023-12-28 23:31:38 +0530

    update: - text processing codes

---
+++ Hackerrank/text_alignment.py
@@ -0,0 +1,32 @@
+"""
+Creating a Hackerrank logo using character 'H'
+Replaced all ______ with rjust, ljust or center suitably. 
+"""
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    # left 'H' ladder with .rjust + 'H' + right ladder with .ljust
+    print((c*i).rjust(thickness-1) + c + (c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    # 1st pillar is centered with thickness*2 spaces + 2nd pillar is centered with thickness*6 times spaces
+    print((c*thickness).center(thickness*2) + (c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    # Belt height is CEIL(thickness/2). Belt width is 5*thickness and is centered with 6*thickness
+    print((c*thickness*5).center(thickness*6))
+
+#Bottom Pillars
+for i in range(thickness+1):
+    # 1st pillar is centered with thickness*2 spaces + 2nd pillar is centered with thickness*6 times spaces
+    print((c*thickness).center(thickness*2) + (c*thickness).center(thickness*6))
+
+#Bottom Cone
+for i in range(thickness):
+    # right 'H' ladder with .rjust + 'H' + left 'H' ladder with .ljust. The whole bottom cone is .rjust with 6*thickness to push towards right pillar
+    print(((c*(thickness-i-1)).rjust(thickness) + c + (c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
---
+++ Hackerrank/text_wrap.py
@@ -0,0 +1,14 @@
+"""
+Question: Given a string and an integer 'max_width'
+            Wrap the the string by keeping only 'max_width' no. of characters in each line
+"""
+
+import textwrap
+
+def wrap(string, max_width):
+    return '\n'.join(textwrap.wrap(string, max_width))
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
