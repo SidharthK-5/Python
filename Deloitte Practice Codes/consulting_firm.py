6fc7cf5f1bf2615fb5fba6fa7be39a70316bb166
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-02 11:37:39 +0530

    update: - consulting firm - conv to df

---
+++ Deloitte Practice Codes/consulting_firm.py
@@ -0,0 +1,22 @@
+"""
+Enter the details of Entries and find the result based on average of 'Marks'
+"""
+
+import pandas as pd
+
+num_entities = int(input())
+details = dict()
+for entity in range(num_entities):
+    details[entity] = input().split()
+
+# Create dataframe from input details
+dataframe = pd.DataFrame(details, index=["Name", "Age", "Marks", "Country"]).T
+dataframe["Marks"] = dataframe["Marks"].astype(int) # Convert 'Marks' column to int type
+
+# Calculate Result based on mean 'Marks' and add the column to dataframe
+Result = [1.0 if i <= dataframe["Marks"].mean() else 0.0 for i in dataframe["Marks"]]
+dataframe["Result"] = Result
+
+# Output the variable to STDOUT
+for entity in dataframe["Result"]:
+    print(entity)
---
+++ Deloitte Practice Codes/convert_to_dataframe.py
@@ -0,0 +1,21 @@
+"""
+Generate a dataframe from given numbers if the matrix order is satisfied
+"""
+
+import pandas as pd
+
+rows, cols = map(int, input().split())
+input_list = input().split()
+
+if rows*cols == len(input_list):
+    # Matrix order is statisfied
+    data = dict()
+    for i in range(rows):
+        # Each row will have 'cols' no. of elements in it
+        row = pd.Series(input_list[:cols])
+        data[i] = row
+        input_list = input_list[cols:]
+    dataframe = pd.DataFrame(data).T
+    print(dataframe)
+else:
+    print("Weird Order!")
