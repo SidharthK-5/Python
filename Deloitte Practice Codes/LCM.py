e8b39ba1e9336be4f307f3e3baa2e425c867f3e0
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-02 21:39:03 +0530

    update: - digital marketing firm - finding lcm

---
+++ Deloitte Practice Codes/LCM.py
@@ -0,0 +1,37 @@
+"""
+Find the LCM of number pairs
+"""
+
+def find_lcm(pair: list[int]) -> int:
+    """
+    Find the LCM of number pair
+
+    Args:
+        pair (list[int]): list of numbers to find LCM
+
+    Returns:
+        int: LCM of two numbers
+    """
+    num_1, num_2 = map(int, pair)
+    if num_1<1 or num_1>1000 or num_2<1 or num_2>1000:
+        return "Out of Range!"
+    elif num_1 == 1 or num_2 == 1:
+        return max(num_1, num_2)
+    else:
+        # Create sets of multiples of the numbers
+        s1 = set(range(num_1, num_1*num_2+1, num_1))
+        s2 = set(range(num_2, num_1*num_2+1, num_2))
+
+        # Create a set of common multiples of these numbers
+        s3 = s1.intersection(s2)
+        return min(s3)
+    
+
+num_of_pairs = int(input())
+list_of_pairs = []
+for _ in range(num_of_pairs):
+    list_of_pairs.append(input().split())
+
+for num_pair in list_of_pairs:
+    lcm = find_lcm(num_pair)
+    print(lcm)
---
+++ Deloitte Practice Codes/digital_marketing_firm.py
@@ -0,0 +1,21 @@
+"""
+Enter the details of Entries and find the result based on average of 'open_rate'
+"""
+
+import pandas as pd
+
+num_entities = int(input())
+campaign_name = input().split()
+no_of_emails = list(map(int, input().split()))
+open_rate = list(map(int, input().split()))
+
+# Create dataframe from input details
+details = {"campaign_name": campaign_name, "no_of_emails": no_of_emails, "open_rate": open_rate}
+dataframe = pd.DataFrame(details)
+mean_of_opening = dataframe["open_rate"].mean() # Find the mean of 'open_rate' values column
+
+for value in dataframe["open_rate"]:
+    if value >= mean_of_opening:
+        print(True)
+    else:
+        print(False)
