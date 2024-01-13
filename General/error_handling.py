fbaf0bd84ba137c7b67af8a03e6ef9d3795d88bc
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-13 12:38:46 +0530

    update: - error handling - word counter

--- General/error_handling.py
+++ General/error_handling.py
@@ -3,34 +3,46 @@ Error handling example with try...except
 """
 
 
-def get_list_item(lis: list[int], index: int) -> int | None:
+def get_list_item(input_list: list[int], index: int) -> int | None:
     """
     Finds if the given index is present in the list or not
 
     Args:
-        lis (list[int]): List in which index is checked
+        input_list (list[int]): List in which index is checked
         index (int): The index to be found
 
     Returns:
         int | None: Value at index or None if index is not present
     """
     try:
-        return lis[index]
+        return input_list[index]
     except IndexError:
         return None
 
 
-def w4(x, y=3):
-    if y < 0:
+def multiply_strings(string: str, multiplier: int = 3) -> str:
+    """
+    Multiply a string give nnumbre of times
+
+    Args:
+        string (str): The string to be multiplied
+        multiplier (int, optional): How many times the string is to be multiplied. Defaults to 3.
+
+    Raises:
+        ValueError: If multiplier is less than 0
+
+    Returns:
+        str: Multiplied string
+    """
+    if multiplier < 0:
         raise ValueError("y must be greater than 0")
 
-
-    z = x * y
-    w = z + "!!"
-    return w
+    multiplied_string = string * multiplier
+    output = multiplied_string + "!!"
+    return output
 
 
 my_list = [1, 2, 3, 4, 5, 6, 7]
-print(get_list_item(lis=my_list, index=8))
+print(get_list_item(input_list=my_list, index=8))
 
-print(w4("Yay", 5))
+print(multiply_strings(string="Yay", multiplier=5))
---
+++ General/word_counter.py
@@ -0,0 +1,8 @@
+"""
+Count the number of words in a given string
+"""
+
+
+string = input()
+count = len(string.split())
+print(count)
