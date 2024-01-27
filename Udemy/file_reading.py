42bf454430927650c11372558eaadf775dc9d9e7
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-01-27 23:00:42 +0530

    update: - file reading and handling methods

--- Udemy/file_reading.py
+++ Udemy/file_reading.py
@@ -1,15 +1,21 @@
-f = open("sample_file.txt", "r")
-# read() will take no of char needed as argument. Else it will read the full file
+"""
+File reading methods
+"""
+
+# Read the file to memory
+file = open("data/sample_file.txt", "r")
+
+# read() will take number of char needed as argument. Else it will read the full file
 # Read will return a string object
-print(f.read(7))
+print(file.read(7))
 
 # readlines() - Read all lines, but return type is list
-content = f.readlines()
+content = file.readlines()
 print(
     content[:1]
 )  # Prints only first line. Since the earlier statement covered upto 7 characters, this statement will print rest of the characters in the line
 
 # readline() - Reads line by line only
-content = f.readline()
+content = file.readline()
 print(content)
 # DO this iteratively
