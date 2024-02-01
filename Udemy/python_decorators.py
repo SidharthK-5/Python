fdfb30e5a7cbea73e85b439bd9cbc759db7adf61
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-02-01 22:25:36 +0530

    update: - python decorators

--- Udemy/python_decorators.py
+++ Udemy/python_decorators.py
@@ -18,11 +18,14 @@ def concat(val1, val2):
     return val1 + val2
 
 
+# val1 and val2 are of same type int. So addition takes place as per function logic
 result = concat(10, 10)
 print(result)
 
+# val1 and val2 are of different type. So decorator converts both variables to string type. Thus concatenation takes place
 result = concat("Python", 10)
 print(result)
 
+# val1 and val2 are both type str. So string addition results in concatenation
 result = concat("Python", "String")
 print(result)
