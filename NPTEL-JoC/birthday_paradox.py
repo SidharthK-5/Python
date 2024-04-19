3203ad8fe57f8c8b1240ad71e4f50917ddfcc841
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-04-20 00:04:57 +0530

    feat: - birthday paradox - formatted dobble game

---
+++ NPTEL-JoC/birthday_paradox.py
@@ -0,0 +1,53 @@
+"""
+Birthday Paradox - Find your twin
+The program generates a list of day-of-year of 50 people (50 numbers)
+From the list, matching day-of-year are declared as twins
+"""
+
+
+import datetime
+import random
+
+birthdays = []
+index = 0
+start_year = 1900  # Assume the oldest person alive is born in 1900
+current_year = int(datetime.date.today().strftime("%Y"))
+
+while index < 50:
+    # Set a random year
+    year = random.randint(start_year, current_year)
+    # Check if the year is leap year or not
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        leap = True
+    else:
+        leap = False
+
+    # Set a random month
+    month = random.randint(1, 12)
+
+    # Set a random day based on month value
+    if month == 2 and leap:
+        day = random.randint(1, 29)
+    elif month == 2 and leap is False:
+        day = random.randint(1, 28)
+    elif month == 7 or month == 8:  # Handling July & August
+        day = random.randint(1, 31)
+    elif month % 2 != 0 and month < 7:
+        day = random.randint(1, 31)
+    elif month % 2 == 0 and month > 8 and month < 12:
+        day = random.randint(1, 31)
+    else:
+        day = random.randint(1, 30)
+
+    converted_date = datetime.date(year=year, month=month, day=day)
+    day_of_year = converted_date.timetuple().tm_yday
+    birthdays.append(day_of_year)
+    index += 1
+birthdays.sort()
+print("The day of year of birthdays of 50 people are:")
+print(f"{birthdays=}")
+
+print("\nTwins are found for:")
+for date in range(1, len(birthdays)):
+    if birthdays[date] == birthdays[date - 1]:
+        print(f"Twin found for {birthdays[date]}")
--- NPTEL-JoC/dobble_game.py
+++ NPTEL-JoC/dobble_game.py
@@ -7,11 +7,11 @@ import random
 
 
 symbols = list(string.ascii_letters)
-card_one = [0]*5
-card_two = [0]*5
+card_one = [0] * 5
+card_two = [0] * 5
 # pos_one and pos_two are same symbol positions in both cards
-pos_one = random.randint(0,4)
-pos_two = random.randint(0,4)
+pos_one = random.randint(0, 4)
+pos_two = random.randint(0, 4)
 
 same_symbol = random.choice(symbols)
 symbols.remove(same_symbol)  # Remove same_symbol to avoid replication
@@ -20,12 +20,12 @@ if pos_one == pos_two:
     # Assign the same symbol to same position
     card_one[pos_one] = same_symbol
     card_two[pos_one] = same_symbol
-    
+
 else:
     # Assign same symbol to respective index positions
     card_one[pos_one] = same_symbol
     card_two[pos_two] = same_symbol
-    
+
     """
     Assign elements to swapped positions of lists.
     Otherwise those indices won't be replaced
@@ -34,9 +34,9 @@ else:
     symbols.remove(card_one[pos_two])
     card_two[pos_one] = random.choice(symbols)
     symbols.remove(card_two[pos_one])
-    
+
 i = 0
-while i<5:
+while i < 5:
     if i != pos_one and i != pos_two:
         """
         Condition satisfies when value of 'i' is neither pos_one nor pos_two
@@ -49,7 +49,7 @@ while i<5:
         card_one[i] = alphabet_one
         card_two[i] = alphabet_two
     i += 1
-    
+
 print(f"{card_one=}")
 print(f"{card_two=}")
 
