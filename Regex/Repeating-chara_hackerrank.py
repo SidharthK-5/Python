91c2cd385b54c5acb0c3d3c91293c54ef8eb892f
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-06-01 22:53:07 +0530

    update: - repeating character hackerrank

---
+++ Regex/Repeating-chara_hackerrank.py
@@ -0,0 +1,16 @@
+"""
+Find the first character in the string that repeats itself.
+If no character repeats, print -1
+"""
+
+import re
+
+"""
+To find if the same character repeats next to it.
+\1+ after the group checks for repition of 1st group
+"""
+m = re.search(r"([a-zA-Z0-9])\1+", input().strip())
+if m:
+    print(m.group(1))
+else:
+    print(-1)
