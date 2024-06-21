cae2f7b4be3d8e7981d64a0aa86cdd9be98e847a
SidharthK-5 <sidharthsidzed55@gmail.com>
2024-06-21 23:05:29 +0530

    update: - formatted datetime basics

--- NPTEL-JoC/datetime_basics.py
+++ NPTEL-JoC/datetime_basics.py
@@ -9,17 +9,14 @@ import datetime
 print("\nBasics of datetime module\n")
 print(f"Today's date: {datetime.date.today()}")
 print(f"Current year: {datetime.date.today().strftime('%Y')}")
-print(
-    f"Current year without century as decimal number: {datetime.date.today().strftime('%y')}"
-)
+print("Current year without century as decimal number: ", end="")
+print(f"{datetime.date.today().strftime('%y')}")
 print(f"Current month: {datetime.date.today().strftime('%B')}")
 print(f"Current date: {datetime.date.today().strftime('%d')}")
-print(
-    f"Week number of this year (Sun as 1st day of the week): {datetime.date.today().strftime('%U')}"
-)
-print(
-    f"Week number of this year (Mon as 1st day of the week): {datetime.date.today().strftime('%W')}"
-)
+print("Week number of this year (Sun as 1st day of the week): ", end="")
+print(f"{datetime.date.today().strftime('%U')}")
+print("Week number of this year (Mon as 1st day of the week): ", end="")
+print(f"{datetime.date.today().strftime('%W')}")
 print(f"Weekday of the week: {datetime.date.today().strftime('%w')}")
 print(f"Day of the year: {datetime.date.today().strftime('%j')}")
 print(f"Day of week: {datetime.date.today().strftime('%A')}")
@@ -27,9 +24,8 @@ print(f"Day of week shortened: {datetime.date.today().strftime('%a')}")
 print(
     f"Date and Time (24 hr): {datetime.datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}"
 )
-print(
-    f"Date and Time (12 hr): {datetime.datetime.now().strftime('%d/%m/%Y, %I:%M:%S %p')}"
-)
+print("Date and Time (12 hr): ", end="")
+print(f"{datetime.datetime.now().strftime('%d/%m/%Y, %I:%M:%S %p')}")
 print(
     f"Locale's appropriate date and time rep: {datetime.datetime.now().strftime('%c')}"
 )
