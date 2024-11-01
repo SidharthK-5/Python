60f15ba118f7b8e20237c75810ed7506c3aa5be3
Sidharth K <sidharthsidzed55@gmail.com>
2024-11-01 14:30:38 +0530

    feat: - added ipykernel dependency for jupyter notebooks - recursion examples

---
+++ General/binary_search_recursion.py
@@ -0,0 +1,26 @@
+"""
+Program to perform binary serach using recursion
+"""
+
+def binary_search(array, low, high, key):
+    if low <= high:
+        mid = (low + high) // 2
+        if array[mid] == key:
+            return mid
+        elif array[mid] > key:
+            return binary_search(array, low, mid - 1, key)
+        else:
+            return binary_search(array, mid + 1, high, key)
+    else:
+        return -1
+    
+
+if __name__ == "__main__":
+    array = list(map(int, input("Enter the array elements: ").split()))
+    target = int(input("Enter the target element: "))
+    array = sorted(array)
+    result = binary_search(array, 0, len(array) - 1, target)
+    if result != -1:
+        print(f"{target} found at index {result}")
+    else:
+        print(f"{target} not found")
